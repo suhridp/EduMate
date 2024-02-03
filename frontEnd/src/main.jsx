7875108b90623be5da156627ab4b9d01099960ea
Suhrid <suhridbpaul@gmail.com>
2024-02-03 12:12:30 +0530

    Created required pages for the web application

--- frontEnd/src/main.jsx
+++ frontEnd/src/main.jsx
@@ -1,10 +1,32 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import Home from "./Home.jsx";
+import Layout from "./Layout.jsx";
+import Aboutus from "./Aboutus.jsx";
+import Profile from "./Profile.jsx";
+import Notes from "./Notes.jsx";
+import {
+  Route,
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<Layout />}>
+      <Route path="" element={<Home />} />
+      <Route path="about" element={<Aboutus />} />
+      <Route path="profile" element={<Profile />} />
+
+      <Route path="notes" element={<Notes />} />
+    </Route>
+  )
+);
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
